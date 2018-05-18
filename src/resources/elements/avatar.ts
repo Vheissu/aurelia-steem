2602c2236a740a3b18d60f1d0ed501d7c5afcd4c
Dwayne Charrington <dwaynecharrington@gmail.com>
2018-05-18 16:41:09 +1000

    feat(avatar): working customisable avatar component with styles

--- src/resources/elements/avatar.ts
+++ src/resources/elements/avatar.ts
@@ -1,13 +1,18 @@
-import { bindable } from 'aurelia-framework';
+import './avatar.scss';
+
+import { bindable, containerless } from 'aurelia-framework';
 
 export const getAvatarURL = (username, size = 100) => size > 64 ? `https://steemitimages.com/u/${username}/avatar` : `https://steemitimages.com/u/${username}/avatar/small`;
 
+@containerless()
 export class Avatar {
     @bindable username;
     @bindable size = 100;
 
+    public style: any = {};
+
     attached() {
-        let style: any = {
+        this.style = {
             minWidth: `${this.size}px`,
             width: `${this.size}px`,
             height: `${this.size}px`
@@ -16,8 +21,8 @@ export class Avatar {
         const url = getAvatarURL(this.username, this.size);
 
         if (this.username) {
-            style = {
-                ...style,
+            this.style = {
+                ...this.style,
                 backgroundImage: `url(${url})`
             };
         }
